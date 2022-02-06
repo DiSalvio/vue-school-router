d70638ecdafc7e9dd059a2e376aa6889c48d860f
DiSalvio <stephendisalvio@protonmail.com>
2022-02-06 16:06:38 +0800

    add routes to travel destinations and basic json data

--- travel-app-vue-cli/src/router/index.js
+++ travel-app-vue-cli/src/router/index.js
@@ -1,5 +1,9 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
+import PageBrazil from "../views/PageBrazil.vue"
+import PagePanama from "../views/PagePanama.vue"
+import PageHawaii from "../views/PageHawaii.vue"
+import PageJamaica from "../views/PageJamaica.vue"
 
 const routes = [
   {
@@ -16,6 +20,26 @@ const routes = [
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue"),
   },
+  {
+    path:"/brazil",
+    name:"Brazil",
+    component: PageBrazil
+  },
+  {
+    path:"/panama",
+    name:"Panama",
+    component: PagePanama
+  },
+  {
+    path:"/hawaii",
+    name:"Hawaii",
+    component: PageHawaii
+  },
+  {
+    path:"/Jamaica",
+    name:"Jamaica",
+    component: PageJamaica
+  }
 ];
 
 const router = createRouter({

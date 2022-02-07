4a89287d33b335e5ee2c1f1df390d7e19540f053
DiSalvio <stephendisalvio@protonmail.com>
2022-02-07 17:27:47 +0800

    lazy load destination pages with vite

--- travel-app/travel-app/src/router/index.js
+++ travel-app/travel-app/src/router/index.js
@@ -1,16 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import PageHome from '@/views/PageHome.vue'
-import PageBrazil from '@/views/PageBrazil.vue'
-import PagePanama from '@/views/PagePanama.vue'
-import PageHawaii from '@/views/PageHawaii.vue'
-import PageJamaica from '@/views/PageJamaica.vue'
 
 const routes = [
     { path: '/', name: 'PageHome', component: PageHome },
-    { path: '/brazil', name: 'PageBrazil', component: PageBrazil },
-    { path: '/panama', name: 'PagePanama', component: PagePanama },
-    { path: '/hawaii', name: 'PageHawaii', component: PageHawaii },
-    { path: '/jamaica', name: 'PageJamaica', component: PageJamaica }
+    { path: '/brazil', name: 'PageBrazil', component: ()=>import('@/views/PageBrazil.vue')},
+    { path: '/panama', name: 'PagePanama', component: ()=>import('@/views/PagePanama.vue')},
+    { path: '/hawaii', name: 'PageHawaii', component: ()=>import('@/views/PageHawaii.vue')},
+    { path: '/jamaica', name: 'PageJamaica', component: ()=>import('@/views/PageJamaica.vue')}
 ]
 
 const router = createRouter({

e95eb1070319ce8c34c88b0aea83cb2c7297e581
DiSalvio <stephendisalvio@protonmail.com>
2022-03-13 10:02:10 +0800

    Soft 404 not found page added

---
+++ travel-app/travel-app/src/views/NotFound.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h1>404 Not Found</h1>
+    <p>
+      Where you're going, you don't need routes.
+      <router-link to="/">Home</router-link>
+    </p>
+  </div>
+</template>
